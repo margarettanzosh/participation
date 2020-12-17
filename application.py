47745edebc67411ff7988a84c69268cbce1865c6
Margaret Tanzosh <margarettanzosh@nestmk12.net>
2020-12-17 17:01:47 -0500

    Update application.py

--- application.py
+++ application.py
@@ -22,6 +22,7 @@ app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
 # Ensure responses aren't cached
